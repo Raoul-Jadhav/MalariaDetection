91394e73fc5ff0b1e327634af926e36cb06c5a34
Rahul Jadhav <76778870+Raoul-Jadhav@users.noreply.github.com>
2021-01-05 15:23:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on Mon Dec 28 15:44:20 2020
+@author: Rahul Jadhav
+"""
+
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+
+# Keras
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH ='model_vgg19.h5'
+
+# Load your trained model
+model = load_model(MODEL_PATH)
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+
+    # Preprocessing the image, converting imageto array
+    x = image.img_to_array(img)    
+    ## Scaling
+    x=x/255
+    x = np.expand_dims(x, axis=0)  
+    preds = model.predict(x)
+    preds=np.argmax(preds, axis=1)
+    if preds==0:
+        preds="The Person is Infected With Malaria"
+    else:
+        preds="The Person is not Infected With Malaria"
+        
+    return preds    
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+        result=preds
+        return result
+    return 'Error'
+
+if __name__ == '__main__':
+    app.run(debug=True)
